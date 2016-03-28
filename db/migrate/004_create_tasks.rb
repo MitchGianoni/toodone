09a6bfc8e7001c7302d85ae1e09a3bf2ea33493d
MitchGianoni <mitchgianoni@yahoo.com>
2016-03-28 17:14:03 -0400

    add tasks table migration

--- db/migrate/004_create_tasks.rb
+++ db/migrate/004_create_tasks.rb
@@ -2,8 +2,9 @@ class CreateTasks < ActiveRecord::Migration
 	def up
 		create_table :tasks do |t|
 			t.string :item, null: false
-			t.datetime :duedate
+			t.datetime :duedate 
 			t.boolean :completed
+			t.integer :list_id, null: false
 		end
 	end
 

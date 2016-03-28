5dd87586e6b9b31daca4404d67cf863a5ba14588
MitchGianoni <mitchgianoni@yahoo.com>
2016-03-28 18:43:17 -0400

    Updated - add, edit, done methods now complete

--- lib/too_done.rb
+++ lib/too_done.rb
@@ -17,38 +17,73 @@ module TooDone
     option :date, :aliases => :d,
       :desc => "A Due Date in YYYY-MM-DD format."
     def add(task)
-      puts "Add task to which list?"
-      list = STDIN.gets.chomp.downcase
-      thislist = List.find_or_create_by!(title: list, user_id: current_user.id)
-      puts "Due Date?(YYYY-MM-DD)"
-      duedate = STDIN.gets.chomp.downcase
-      completed = false
-      listid = thislist.id
-      thistask = Task.find_or_create_by!(item: task, duedate: duedate, completed: completed, list_id: listid)
+      thislist = List.find_or_create_by(title: options[:list], user_id: current_user.id)
+      thistask = Task.find_or_create_by!(item: task, duedate: options[:date], completed: false, list_id: thislist.id)
+      puts "Added task #{thistask.item} to #{thislist.title}"
     end
 
     desc "edit", "Edit a task from a todo list."
     option :list, :aliases => :l, :default => "*default*",
       :desc => "The todo list whose tasks will be edited."
     def edit
-
-      # find the right todo list
-      # BAIL if it doesn't exist and have tasks
-      # display the tasks and prompt for which one to edit
-      # allow the user to change the title, due date
+      list = List.find_by(title: options[:list], user_id: current_user.id)
+      unless list
+        puts "No such list. #{options[:list]}"
+        exit
+      end
+      task = Task.where(list_id: list.id)
+      unless task.count > 0
+        puts "No tasks on this list"
+        exit
+      end
+      task.each do |x|
+        puts "#{x.id}, #{x.item}, #{x.duedate}, #{x.completed}, #{x.list_id}, \n"
+      end
+      puts "Edit which task?(enter task ID)"
+      edit = STDIN.gets.chomp.to_i
+      editor = Task.find_by(id: edit)
+      puts "Update Title or Due Date? (T/D)"
+      select = STDIN.gets.chomp.downcase
+      unless select == 't' || 'd'
+        puts "Wrong move idjit"
+        exit
+      end
+      if select == "t"
+        puts "New Title?"
+        title = STDIN.gets.chomp.downcase
+        editor.update(item: "#{title}")
+        puts "New Title is now #{editor.item}"
+      elsif select == "d"
+        puts "New Due Date? (YYYY-MM-DD)"
+        date = STDIN.gets.chomp.downcase
+        editor.update(duedate: date)
+        puts "New Due Date is now #{editor.duedate}"
+      end
     end
 
     desc "done", "Mark a task as completed."
     option :list, :aliases => :l, :default => "*default*",
       :desc => "The todo list whose tasks will be completed."
     def done
-      #puts "What list is the task on?"
-      #list = STDIN.gets.chomp.downcase
-     # thislist = List.find_by!(title: list, user_id: current_user.id)
-
-      # find the right todo list
-      # BAIL if it doesn't exist and have tasks
-      # display the tasks and prompt for which one(s?) to mark done
+      list = List.find_by(title: options[:list], user_id: current_user.id)
+      unless list
+        puts "No such list. #{options[:list]}"
+        exit
+      end
+      task = Task.where(list_id: list.id)
+      unless task.count > 0
+        puts "No tasks on this list"
+        exit
+      end
+      puts "Tasks in this list:"
+      task.each do |x|
+        puts "#{x.id}, #{x.item}, #{x.duedate}, #{x.completed}, #{x.list_id}, \n"
+      end
+      puts "Which task is complete?(enter task ID)"
+      done = STDIN.gets.chomp.to_i
+      donezo = Task.find_by(id: done)
+      donezo.update(completed: true)
+      puts "Task #{donezo.item} is now marked complete"
     end
 
     desc "show", "Show the tasks on a todo list in reverse order."
@@ -60,6 +95,9 @@ module TooDone
       :desc => "Sorting by 'history' (chronological) or 'overdue'.
       \t\t\t\t\tLimits results to those with a due date."
     def show
+     # options[:sort] == "history"
+     #  ...
+     #elsif options[:sort]
       # find or create the right todo list
       # show the tasks ordered as requested, default to reverse order (recently entered first)
     end
@@ -96,3 +134,18 @@ TooDone::App.start(ARGV)
 # add
       # find or create the right todo list
       # create a new item under that list, with optional date
+        # Alternate code:
+        # => puts "Add task to which list?"
+        # => list = STDIN.gets.chomp.downcase
+        # => thislist = List.find_or_create_by!(title: list, user_id: current_user.id)
+        # => puts "Due Date?(YYYY-MM-DD)"
+        # => duedate = STDIN.gets.chomp.downcase
+# done
+      # find the right todo list
+      # BAIL if it doesn't exist and have tasks
+      # display the tasks and prompt for which one(s?) to mark done
+# edit
+      # find the right todo list
+      # BAIL if it doesn't exist and have tasks
+      # display the tasks and prompt for which one to edit
+      # allow the user to change the title, due date

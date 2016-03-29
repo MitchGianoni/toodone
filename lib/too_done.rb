5050f75b2faa0e753ecff09711106ef09a10b239
MitchGianoni <mitchgianoni@yahoo.com>
2016-03-29 11:44:07 -0400

    DELETE and SHOW methods complete :) everything complete

--- lib/too_done.rb
+++ lib/too_done.rb
@@ -44,7 +44,7 @@ module TooDone
       editor = Task.find_by(id: edit)
       puts "Update Title or Due Date? (T/D)"
       select = STDIN.gets.chomp.downcase
-      unless select == 't' || 'd'
+      unless select == 't' || select == 'd'
         puts "Wrong move idjit"
         exit
       end
@@ -95,11 +95,17 @@ module TooDone
       :desc => "Sorting by 'history' (chronological) or 'overdue'.
       \t\t\t\t\tLimits results to those with a due date."
     def show
-     # options[:sort] == "history"
-     #  ...
-     #elsif options[:sort]
-      # find or create the right todo list
-      # show the tasks ordered as requested, default to reverse order (recently entered first)
+      this_list = List.find_or_create_by(title: options[:list], user_id: current_user.id)
+      if options[:sort] == "history"
+        tasks = Task.where("list_id = ? AND completed = ?", this_list.id, options[:completed]).order(id: :asc)
+      elsif options[:sort] == "overdue"
+        tasks = Task.where("list_id = ? AND duedate < ? AND completed = ? ", this_list.id, DateTime.now, options[:completed]).order(id: :desc)
+      else 
+        tasks = Task.where("list_id = ? AND completed = ?",this_list.id, options[:completed]).order(id: :desc)
+      end
+      tasks.each do |x|
+        puts "#{x.item}, due: #{x.duedate}, completed: #{x.completed}."
+      end
     end
 
     desc "delete [LIST OR USER]", "Delete a todo list or a user."
@@ -107,12 +113,52 @@ module TooDone
       :desc => "The todo list which will be deleted (including items)."
     option :user, :aliases => :u,
       :desc => "The user which will be deleted (including lists and items)."
-    def delete
-      # BAIL if both list and user options are provided
-      # BAIL if neither list or user option is provided
-      # find the matching user or list
-      # BAIL if the user or list couldn't be found
-      # delete them (and any dependents)
+    def delete 
+      if options[:user] != nil && options[:list] != "*default*"
+        puts "Cannot modify both simultaneously, idjit."
+        exit
+      elsif options[:user] == nil && options[:list] == "*default*"
+        puts "You didn't provide anything, idjit."
+        exit
+      end
+      if options[:list] != "*default*"
+        list = List.find_by(title: options[:list])
+        if list == nil
+          puts "List not found idjit"
+          exit
+        end
+        tasks = Task.where(list_id: list.id)
+        tasks.each  do |x|
+          puts "#{x.item} task deleted."
+          x.destroy
+          end
+        puts "#{list.title} list deleted."
+        list.destroy
+      end
+      if options[:user] != nil
+        user = User.find_by(name: options[:user])
+        if user == nil
+          puts "User not found idjit"
+          exit
+        end
+        user_lists = List.where(user_id: user.id)
+        if user_lists == []
+          puts "No lists remaining idjit"
+          exit
+        else
+          user_lists.each do |x|
+            tasks = Task.where(list_id: x.id)
+            tasks.each do |y|
+              puts "#{y.item} task deleted."
+              y.destroy
+            end
+            puts "#{x.title} list deleted."
+            x.destroy
+          end
+          puts "#{user.name} user deleted."
+          user.destroy
+        end
+      end
     end
 
     desc "switch USER", "Switch session to manage USER's todo lists."
@@ -149,3 +195,12 @@ TooDone::App.start(ARGV)
       # BAIL if it doesn't exist and have tasks
       # display the tasks and prompt for which one to edit
       # allow the user to change the title, due date
+# delete
+      # BAIL if both list and user options are provided
+      # BAIL if neither list or user option is provided
+      # find the matching user or list
+      # BAIL if the user or list couldn't be found
+      # delete them (and any dependents)
+# show
+      # find or create the right todo list
+      # show the tasks ordered as requested, default to reverse order (recently entered first)
